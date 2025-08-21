7456a3a184166a01443da8c083b1cff8563b8419
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-22 02:59:24 +0300

    api testing for updateBook endpoint

--- src/test/integration/Books-api.test.ts
+++ src/test/integration/Books-api.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 
 import prisma from "../../config/prisma.config";
 import server from "../../server";
+import { title } from "process";
 
 beforeEach(async () => {
   await prisma.book.deleteMany();
@@ -9,12 +10,14 @@ beforeEach(async () => {
 
 afterAll(async () => {
   server.close();
+  await prisma.book.deleteMany();
 });
 
 describe("getBook", () => {
   it("should return 200 and get book from db", async () => {
     const book = await prisma.book.create({ data: { title: "MyBook" } });
     const res = await request(server).get(`/api/books/${book.id}`);
+
     expect(res.status).toBe(200);
     expect(res.body.message).toMatch("book retrieved successfully");
     expect(res.body.data.book).toMatchObject({ title: "MyBook" });
@@ -24,6 +27,29 @@ describe("getBook", () => {
     const res = await request(server).get(
       `/api/books/43556608-4797-4416-b9b5-f1e6945107dd`
     );
+
+    expect(res.status).toBe(404);
+    expect(res.body.message).toMatch("404 not found");
+  });
+});
+
+describe("updateBook", () => {
+  it("should return 200 and get book from db", async () => {
+    const book = await prisma.book.create({ data: { title: "MyBook" } });
+    const res = await request(server)
+      .put(`/api/books/${book.id}`)
+      .send({ title: "MyBook Updated" });
+
+    expect(res.status).toBe(200);
+    expect(res.body.message).toMatch("book updated successfully");
+    expect(res.body.data.book).toMatchObject({ title: "MyBook Updated" });
+  });
+
+  it("should return 404 if book not found", async () => {
+    const res = await request(server)
+      .put(`/api/books/43556608-4797-4416-b9b5-f1e6945107dd`)
+      .send({ title: "MyBook Updated" });
+
     expect(res.status).toBe(404);
     expect(res.body.message).toMatch("404 not found");
   });
