8e00bd6ccd917413bc5db8fe76b0732db9ff6a63
SamerYaserr <samerzagloul@std.mans.edu.eg>
2025-08-21 17:11:59 +0300

    refactor

--- src/modules/Book/Book.service.ts
+++ src/modules/Book/Book.service.ts
@@ -1,4 +1,4 @@
-import prisma from "../../config/prisma";
+import prisma from "../../config/prisma.config";
 import { ICreateBookBody, IUpdateBookBody, IBook } from "./Book.interface";
 
 class BookService {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-import env from "./config/env";
+import env from "./config/env.config";
 
 const PORT = env.PORT || 3000;
 
